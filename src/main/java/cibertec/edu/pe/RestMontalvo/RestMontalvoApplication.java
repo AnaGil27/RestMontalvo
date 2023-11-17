1488178f17bcf7d688117232cee2d12f1b8c5054
AnaGil27 <gilyalta27@gmail.com>
2023-11-17 16:09:28 -0500

    Commit de creación

---
+++ src/main/java/cibertec/edu/pe/RestMontalvo/RestMontalvoApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.RestMontalvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMontalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMontalvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/RestMontalvo/RestMontalvoApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.RestMontalvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMontalvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
