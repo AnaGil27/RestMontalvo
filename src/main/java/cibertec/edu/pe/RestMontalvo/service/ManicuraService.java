ab00995e5b90333ca031957565f80c0a5aee8873
MarcoB2 <begazomarcobegazo@gmail.com>
2023-12-02 01:04:01 -0500

    Primero de Begaaaazo :D

--- src/main/java/cibertec/edu/pe/RestMontalvo/service/ManicuraService.java
+++ src/main/java/cibertec/edu/pe/RestMontalvo/service/ManicuraService.java
@@ -3,12 +3,14 @@ package cibertec.edu.pe.RestMontalvo.service;
 import cibertec.edu.pe.RestMontalvo.model.bd.DescManicura;
 import cibertec.edu.pe.RestMontalvo.model.bd.Manicura;
 
+import cibertec.edu.pe.RestMontalvo.model.bd.Peinado;
 import cibertec.edu.pe.RestMontalvo.model.request.ManicuraRequest;
 import cibertec.edu.pe.RestMontalvo.repository.ManicuraRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Service
@@ -20,6 +22,18 @@ public class ManicuraService {
         return manicuraRepository.findAll();
     }
 
+    public Optional<Manicura> obtenerManicuraPorId(Integer id) {
+        Optional<Manicura> manicura = manicuraRepository.findById(id);
+        return manicura;
+    }
+
+    public void actualizarManicura(Manicura manicura) {
+        manicuraRepository.save(manicura);
+    }
+
+    public Manicura guardar(Manicura manicura){
+        return manicuraRepository.save(manicura);
+    }
     public Manicura guardarManicura(ManicuraRequest manicura){
         Manicura objManicura = new Manicura();
         if(manicura.getIdmanicura() > 0){
@@ -35,6 +49,14 @@ public class ManicuraService {
         return manicuraRepository.save(objManicura);
     }
 
+    public void eliminarManicura(Integer id) {
+        Optional<Manicura> opcionalManicura = manicuraRepository.findById(id);
+        if (opcionalManicura.isPresent()) {
+            manicuraRepository.deleteById(id);
+        } else {
+            throw new IllegalArgumentException("No se encontró el registro con el ID: " + id);
+        }
+    }
 }
 
 
